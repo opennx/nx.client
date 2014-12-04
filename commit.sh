b44d0e186cee280e4398a14abc29454cac7d29d8
Martastain <martas@imm.cz>
2014-12-04 22:45:38 +0100

    pre-commit script

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+chmod 644 LICENSE
+chmod 644 README.md
+
+chmod 644 *.py
+chmod 644 *.css
+chmod 755 firefly.py
+
+chmod 755 images
+chmod 644 images/*.*
+
+chmod 755 nx
+chmod 644 nx/*.py
+
+chmod 755 nx/common
+chmod 644 nx/common/*.py
