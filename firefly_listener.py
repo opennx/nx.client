4b75bf8d6910834a8cde28235cef6e5e8f0cf98f
Martastain <martas@imm.cz>
2016-06-23 08:03:08 +0200

    fixed http listener

--- firefly_listener.py
+++ firefly_listener.py
@@ -32,6 +32,7 @@ class SeismicListener(QThread):
         self.start()
 
     def run(self):
+        logging.info("Starting Seismic listener")
         self.halted = False
         self.last_msg = time.time()
         while not self._halt:
@@ -60,7 +61,7 @@ class SeismicListener(QThread):
             )
 
         try:
-            request = requests.post(
+            request = requests.get(
                     url,
                     stream=True,
                 )
@@ -77,35 +78,35 @@ class SeismicListener(QThread):
 
     def parse_message(self, data, addr=False):
         try:
-            message = SeismicMessage(json.loads(data.decode('ascii')))
-            if message.site_name != self.site_name:
-                return
-            if addr:
-                message.address = addr
-            self.last_msg = time.time()
-
-
-            if message.method == "objects_changed":
-                for i, m in enumerate(self.queue):
-                    if m.method == "objects_changed" and m.data["object_type"] == message.data["object_type"]:
-                        r = list(set(m.data["objects"] + message.data["objects"] ))
-                        self.queue[i].data["objects"] = r
-                        break
-                else:
-                    self.queue.append(message)
-
-            elif message.method == "playout_status":
-                for i, m in enumerate(self.queue):
-                    if m.method == "playout_status":
-                        self.queue[i] = message
-                        break
-                else:
-                    self.queue.append(message)
-
-            else:
-                self.queue.append(message)
+            message = SeismicMessage(json.loads(data))
         except:
             logging.debug("Malformed seismic message detected: {}".format(data))
+            return
+
+        if message.site_name != self.site_name:
+            return
+        if addr:
+            message.address = addr
+        self.last_msg = time.time()
+
+        if message.method == "objects_changed":
+            for i, m in enumerate(self.queue):
+                if m.method == "objects_changed" and m.data["object_type"] == message.data["object_type"]:
+                    r = list(set(m.data["objects"] + message.data["objects"] ))
+                    self.queue[i].data["objects"] = r
+                    break
+            else:
+                self.queue.append(message)
+
+        elif message.method == "playout_status":
+            for i, m in enumerate(self.queue):
+                if m.method == "playout_status":
+                    self.queue[i] = message
+                    break
+            else:
+                self.queue.append(message)
+        else:
+            self.queue.append(message)
 
 
     def halt(self):
